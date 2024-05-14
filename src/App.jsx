9fcc17177868fc1505b5fdcf0d2c94005acff09d
Jesse <chzn1meta@gmail.com>
2024-05-14 13:36:59 -0600

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -14,46 +14,103 @@ function App() {
   const [contactNumber, setContactNumber] = useState("");
 
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value)
+    setFirstName(event.target.value);
   }
 
   
   
   function handleLastNameChange(event) {
-    setLastName(event.target.value)
+    setLastName(event.target.value);
   }
 
 
 
   function handleCurrentAgeChange(event) {
-    setCurrentAge(event.target.value)
+    setCurrentAge(event.target.value);
   }
 
 
 
   function handleCurrentWeightChange(event) {
-    setCurrentWeight(event.target.value)
+    setCurrentWeight(event.target.value);
   }
 
 
 
   function handleGoalWeightChange(event) {
-    setGoalWeight(event.target.value)
+    setGoalWeight(event.target.value);
   }
 
 
 
   function handlePreferedDaysChange(event) {
-    setPreferedDays(event.target.value)
+    setPreferedDays(event.target.value);
   }
 
 
 
   function handleContactNumberChange(event) {
-    setContactNumber(event.target.value)
+    setContactNumber(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formValues = {
+      firstName: firstName,
+      lastName: lastName,
+      currentAge: currentAge,
+      currentWeight: currentWeight,
+      goalWeight: goalWeight,
+      preferedDays: preferedDays,
+      contactNumber: contactNumber
+    };
+    const isValid = validate(formValues);
+    if (isValid) {
+      alert ("Submitted");
+    } else {
+      alert("Fill out Form!");
+    }
+  }
 
+    function validate(formValues) {
+      const currentAgeNum = isNaN(parseInt(formValues.currentAge, 10));
+      console.log(currentAgeNum)
+      if (currentAgeNum) {
+        return false;
+      }
+      const currentWeightNum = isNaN(parseInt(formValues.currentWeightNum, 10));
+      console.log(currentWeightNum)
+      if (currentWeightNum) {
+        return false;
+      }
+      const currentGoalNum = isNaN(parseInt(formValues.currentGoalNum, 10));
+      console.log(currentGoalNum)
+      if (currentGoalNum) {
+        return false;
+      }
+      const preferedDaysNum = isNaN(parseInt(formValues.preferedDaysNum, 10));
+      console.log(preferedDaysNum)
+      if (preferedDaysNum) {
+        return false;
+      }
+      const contactNum = isNaN(parseInt(formValues.contactNum, 10));
+      console.log(contacttNum)
+      if (contactNum) {
+        return false;
+      }
+      if (formValues.firstName.trim() === "" ||
+          formValues.lastName.trim() === "" ||
+          formValues.currentAge.trim() === "" ||
+          formValues.currentWeight.trim() === ""||
+          formValues.goalWeight.trim() === "" ||
+          formValues.preferedDays.trim() === "" ||
+          formValues.contactNumber.trim() === ""
+          )  {
+            return false;
+      }
+        return true;
+    
+    }
 
   return (
     <>
@@ -65,41 +122,41 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Learning React</h1>
+      <h1>Questionnaire</h1>
     
 
-
-      <label for="fname">First Name</label>
+    <form action="#" onSubmit={handleSubmit}>
+      <label htmlFor="fname">First Name</label>
       <input onChange={handleFirstNameChange} type="text" id="fname" name="fname"/>
 
       <br />
 
-      <label for="lname">Last Name</label>
+      <label htmlFor="lname">Last Name</label>
       <input onChange={handleLastNameChange}  type="text" id="lname" name="lname"/>
 
       <br />
 
-      <label for="age">Current Age</label>
-      <input onChange={handleCurrentAgeChange} type="text" id="age" name="age"/>
+      <label htmlFor="age">Current Age</label>
+      <input onChange={handleCurrentAgeChange} type="number" id="age" name="age"/>
 
       <br />
 
-      <label for="weight">Current Weight</label>
+      <label htmlFor="weight">Current Weight</label>
       <input onChange={handleCurrentWeightChange} type="text" id="weight" name="weight"/>
 
       <br />
 
-      <label for="goal">Weight Goal</label>
+      <label htmlFor="goal">Weight Goal</label>
       <input onChange={handleGoalWeightChange} type="text" id="goal" name="goal"/>
 
       <br />
 
-      <label for="days">Prefered Days</label>
+      <label htmlFor="days">Prefered Days</label>
       <input onChange={handlePreferedDaysChange} type="text" id="days" name="days"/>
 
       <br />
 
-      <label for="contact">Contact Number</label>
+      <label htmlFor="contact">Contact Number</label>
       <input onChange={handleContactNumberChange} type="text" id="contact" name="contact"/>
       
       <p>Name: {firstName} {lastName}</p>
@@ -109,8 +166,8 @@ function App() {
       <p>Prefered Days of Exercise: {preferedDays}</p>
       <p>Contact Number: {contactNumber}</p>
 
-      <button>Submit</button>
-
+      <button type="submit">Submit</button>
+    </form>
     </>
   )
 }
